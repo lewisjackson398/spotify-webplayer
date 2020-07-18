bec8225cbf8284463887c4f7acac90be6f6e1095
lewisjackson398 <lewisjackson398@gmail.com>
2020-07-18 01:41:18 +0100

    update link

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "880f3922e94d4edfbda22b8349231d60";
-export const redirectUri = "http://localhost:3000/callback";
+export const redirectUri = "https://lewisjackson398.github.io/spotify-webplayer/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
