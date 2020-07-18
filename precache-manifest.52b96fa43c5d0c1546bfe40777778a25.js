ee969571584842c46a1558733341d299c76ac107
lewisjackson398 <lewisjackson398@gmail.com>
2020-07-18 01:36:48 +0100

    Updates

---
+++ precache-manifest.52b96fa43c5d0c1546bfe40777778a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30361044b8a4053900426ca0666d7966",
+    "url": "/spotify-webplayer/index.html"
+  },
+  {
+    "revision": "f9428a9aaf661ce99e56",
+    "url": "/spotify-webplayer/static/css/main.375837db.chunk.css"
+  },
+  {
+    "revision": "a6e89c31d939ed63bef9",
+    "url": "/spotify-webplayer/static/js/2.0172ddb1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/spotify-webplayer/static/js/2.0172ddb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9428a9aaf661ce99e56",
+    "url": "/spotify-webplayer/static/js/main.6925cef5.chunk.js"
+  },
+  {
+    "revision": "9fbaa452e0d4e3dc4a9f",
+    "url": "/spotify-webplayer/static/js/runtime-main.e2fe8855.js"
+  }
+]);
